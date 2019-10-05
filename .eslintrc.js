ad7eea23261c14f926da9eb09e1ea19e7fd0ef6a
Sanctuary <sanc.pw@gmail.com>
2019-10-05 16:04:45 -0400

    Install/setup eslint-config-sora

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,7 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+	'extends': 'sora/vue',
+	env: {
+		browser: true,
+		node: true,
+	},
+};
