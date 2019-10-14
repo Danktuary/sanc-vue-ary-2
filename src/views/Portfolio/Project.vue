24a23db6f4edbb860a469652d288bb30ba40aaeb
Sanctuary <sanc.pw@gmail.com>
2019-10-14 00:10:50 -0400

    Add basic Project view layout

--- src/views/Portfolio/Project.vue
+++ src/views/Portfolio/Project.vue
@@ -1,6 +1,24 @@
 <template>
-	<section class="project-details">
-		<img :src="project.images[0]" :alt="project.name" :title="project.name" />
+	<section class="project-container">
+		<div class="project-details">
+			<h2 class="project-title">
+				{{ project.name }}
+			</h2>
+			<div class="project-description">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quas quos nisi cum laboriosam.
+			</div>
+		</div>
+		<photoswipe class="project-images">
+			<div
+				v-for="image in project.images"
+				:key="image"
+				v-pswp="image"
+				:style="{ backgroundImage: `url('${image}')` }"
+				class="project-image"
+				data-aos="zoom-out"
+				data-aos-duration="500"
+			></div>
+		</photoswipe>
 	</section>
 </template>
 
@@ -16,8 +34,46 @@ export default {
 };
 </script>
 
-<style>
-.project-details img {
-	border-radius: 4px;
-}
+<style lang="sass">
+@import '../../assets/sass/variables'
+
+.project-container
+	.project-details
+		.project-title
+			display: inline-block
+			font-weight: 700
+			font-size: 2rem
+			padding: 0 0.2em 0.2em
+			border-bottom: 0.25rem solid rgba($blue, 0.75)
+			border-radius: 4px
+
+		.project-description
+			color: lighten($gray, 35%)
+			margin: 1em 0
+
+	.project-images
+		display: flex
+		justify-content: space-around
+		flex-wrap: wrap
+
+		.project-image
+			background-position: center
+			background-size: cover
+			width: 32%
+			height: 200px
+			padding: 1%
+			margin: 1em 0
+			box-sizing: border-box
+			border-radius: 4px
+			box-shadow: 1px 1px 5px 2px rgba(#000, 0.15)
+
+			@media screen and (max-width: 768px)
+				width: 48%
+				height: 150px
+
+			@media screen and (max-width: 490px)
+				height: 100px
+
+		img
+			border-radius: 4px
 </style>
